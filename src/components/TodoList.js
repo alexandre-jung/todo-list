9a61ad65cda2690725e5e2f8e4ae342726def575
Alexandre Jung <jungalexandre@ymail.com>
2021-06-09 17:07:21 +0200

    Setup project structure

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+export default class TodoList extends React.Component {
+
+    render() {
+        return (
+            <div className='TodoList'>
+            </div>
+        );
+    }
+}
